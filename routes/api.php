3b688da1a82eb4b7780cb2306ab0095e9c928c6a
Seyhaz-Dev <seyhogaa@gmail.com>
2026-04-03 09:54:04 +0700

    not yet migration

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\PropertyController;
+use App\Http\Controllers\Api\UnitController;
+use App\Http\Controllers\Api\TenantController;
+use App\Http\Controllers\Api\LeaseController;
+use App\Http\Controllers\Api\PaymentController;
+use App\Http\Controllers\Api\MaintenanceRequestController;
+
+Route::apiResource('properties', PropertyController::class);
+Route::apiResource('units', UnitController::class);
+Route::apiResource('tenants', TenantController::class);
+Route::apiResource('leases', LeaseController::class);
+Route::apiResource('payments', PaymentController::class);
+Route::apiResource('maintenance-requests', MaintenanceRequestController::class);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
